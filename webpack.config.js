cb781c9faf1127f1a6955b80c0c0a3ae9ef84f9b
Wung Jae Lee <wunglee2019@u.northwestern.edu>
2017-07-05 13:29:48 +0900

    Added Countdown and Timer components & styles

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,8 @@ module.exports = {
     alias: {
       Main: 'app/components/Main.jsx',
       Nav: 'app/components/Nav.jsx',
+      Countdown: 'app/components/Countdown.jsx',
+      Timer: 'app/components/Timer.jsx',
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
